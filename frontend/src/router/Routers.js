65914e2181ca37c3b70ba34d6f9c1aabbc1f0c21
Billyrosecodes <tolulopeadeyemi2020@gmail.com>
2023-07-31 12:46:30 +0100

    setting up the router and routing

--- frontend/src/router/Routers.js
+++ frontend/src/router/Routers.js
@@ -14,9 +14,14 @@ const Routers = () => {
   return (
      <Routes>
         <Route path='/' element={<Navigate to='/home'/>} />
-        <Route path='/home'/>
+        <Route path='/home' element={<Home />} />
+        <Route path='/tours' element={<Tours />} />
+        <Route path='/tours/:id' element={<TourDetails />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/tours/search' element={<SearchResultList />} />
      </Routes>
-  )
-}
+  );
+};
 
 export default Routers
