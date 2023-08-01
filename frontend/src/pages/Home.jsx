3403f046bc4e83be60682c6b55715c9aa1e0fde6
Billyrosecodes <tolulopeadeyemi2020@gmail.com>
2023-08-01 08:37:17 +0100

    adding in the hero section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
+import "../styles/home.css";
+
+import { Container, Row, Col } from "reactstrap";
+import heroImg from "../assets/images/hero-img01.jpg";
+import heroImg02 from "../assets/images/hero-img02.jpg";
+import heroVideo from "../assets/images/hero-video.mp4";
+import worldImg from "../assets/images/world.png";
+import Subtitle from "./../shared/Subtitle";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      {/* =========Hero Section Start ====== */}
+      <section>
+        <Container>
+          <Row>
+            <Col lg="6">
+              <div className="hero_content">
+                <div className="hero_subtitle d-flex align-items-center">
+                  <Subtitle Subtitle={"Know Before You Go"} />
+                  <img src={worldImg} alt="" />
+                </div>
+                <h1>
+                  Traveling opens the door to creating
+                  <span className="highlight"> memories</span>
+                </h1>
+                <p>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Rerum praesentium ex vero necessitatibus sit, voluptatum
+                  tempore maxime ratione explicabo corporis consequuntur
+                  assumenda libero facere animi quos a totam ipsam quia?
+                </p>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </>
+  );
+};
 
-export default Home
+export default Home;
