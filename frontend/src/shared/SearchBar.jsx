da9409d4b62dee5a4d7d3057aec7d64d03571eab
Billyrosecodes <tolulopeadeyemi2020@gmail.com>
2023-08-02 01:13:06 +0100

    adding the input container housing the filter by distance,location search functionlity

---
+++ frontend/src/shared/SearchBar.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import "./search-bar.css";
+import { Col, Form, FormGroup } from "reactstrap";
+
+const SearchBar = () => {
+  return (
+    <Col lg="12">
+      <div className="search_bar">
+        <Form className="d-flex align-items-center gap-4">
+          <FormGroup className="d-flex gap-3 form_group form_group-fast">
+            <span>
+              <i class="ri-map-pin-line"></i>
+            </span>
+            <div>
+                <h6>Location</h6>
+                <input type="text" placeholder="Where are you going?"></input>
+            </div>
+          </FormGroup>
+          <FormGroup className="d-flex gap-3 form_group form_group-fast">
+            <span>
+              <i class="ri-map-pin-line"></i>
+            </span>
+            <div>
+                <h6>Distance</h6>
+                <input type="text" placeholder="Distance k/m"></input>
+            </div>
+          </FormGroup>
+          <FormGroup className="d-flex gap-3 form_group form_group-last">
+            <span>
+              <i class="ri-map-pin-line"></i>
+            </span>
+            <div>
+                <h6>Max People</h6>
+                <input type="number" placeholder="0"></input>
+            </div>
+          </FormGroup>
+        </Form>
+      </div>
+    </Col>
+  );
+};
+
+export default SearchBar;
