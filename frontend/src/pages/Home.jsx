c3b29ef54be7587ff05504e61c2a8ccba74426da
Billyrosecodes <tolulopeadeyemi2020@gmail.com>
2023-08-08 19:46:27 +0100

    adding in the img for the experience section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,6 +6,7 @@ import heroImg from "../assets/images/hero-img01.jpg";
 import heroImg02 from "../assets/images/hero-img02.jpg";
 import heroVideo from "../assets/images/hero-video.mp4";
 import worldImg from "../assets/images/world.png";
+import experienceImg from "../assets/images/experience.png";
 import Subtitle from "./../shared/Subtitle";
 
 import SearchBar from "../shared/SearchBar";
@@ -117,7 +118,11 @@ const Home = () => {
                 </div>
               </div>
             </Col>
-            <Col lg="6"></Col>
+            <Col lg="6">
+              <div className="experience_img">
+                <img src={experienceImg} alt="" />
+              </div>
+            </Col>
           </Row>
         </Container>
       </section>
