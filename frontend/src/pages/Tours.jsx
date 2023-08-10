f238743f14ff5da6bf95da3b864c108ebf4e9e15
Billyrosecodes <tolulopeadeyemi2020@gmail.com>
2023-08-10 16:18:32 +0100

    adding in pagination feature

--- frontend/src/pages/Tours.jsx
+++ frontend/src/pages/Tours.jsx
@@ -13,7 +13,7 @@ const Tours = () => {
   const [page, setPage] = useState(0);
 
   useEffect(() => {
-    const pages = Math.ceil(5 / 8); //later we will use backend data count
+    const pages = Math.ceil(5 / 4); //later we will use backend data count
     setPageCount(pages);
   }, [page]);
 
@@ -31,10 +31,24 @@ const Tours = () => {
         <Container>
           <Row>
             {tourData?.map((tour) => (
-              <Col lg="3" key={tour.id}>
+              <Col lg="3" className="mb-4" key={tour.id}>
                 <TourCard tour={tour} />
               </Col>
             ))}
+
+            <Col lg="12">
+              <div className="pagination d-flex align-items-center justify-content-center mt-4 gap-3">
+                {[...Array(pageCount).keys()].map(number => (
+                  <span 
+                   key={number} 
+                   onClick={() => setPage(number)}
+                   className={page === number ? "active_page" : ''}
+                  >
+                    {number + 1}
+                  </span>
+                ))}
+              </div>
+            </Col>
           </Row>
         </Container>
       </section>
