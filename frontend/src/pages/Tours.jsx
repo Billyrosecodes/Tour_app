3aa553d2b9653ef1a69c307e372ff4d0ff8e9acb
Billyrosecodes <tolulopeadeyemi2020@gmail.com>
2023-08-10 14:18:26 +0100

    implementing the tour component

--- frontend/src/pages/Tours.jsx
+++ frontend/src/pages/Tours.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import CommonSection from "../shared/CommonSection";
 
 import "../styles/tour.css";
@@ -6,11 +6,38 @@ import tourData from "../assets/data/tours";
 import TourCard from "./../shared/TourCard";
 import SearchBar from "./../shared/SearchBar";
 import Newsletter from "./../shared/Newsletter";
+import { Container, Row, Col } from "reactstrap";
 
 const Tours = () => {
+  const [pageCount, setPageCount] = useState(0);
+  const [page, setPage] = useState(0);
+
+  useEffect(() => {
+    const pages = Math.ceil(5 / 8); //later we will use backend data count
+    setPageCount(pages);
+  }, [page]);
+
   return (
     <>
       <CommonSection title={"All Tours"} />
+      <section>
+        <Container>
+          <Row>
+            <SearchBar />
+          </Row>
+        </Container>
+      </section>
+      <section className="pt-0">
+        <Container>
+          <Row>
+            {tourData?.map((tour) => (
+              <Col lg="3" key={tour.id}>
+                <TourCard tour={tour} />
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      </section>
     </>
   );
 };
