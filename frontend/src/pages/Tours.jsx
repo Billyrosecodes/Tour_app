2bd9206ed2ba24fe3fa21f67824c78dcdf6902d6
Billyrosecodes <tolulopeadeyemi2020@gmail.com>
2023-08-10 04:30:03 +0100

    importing the neccesary module in the Tour.jsx

--- frontend/src/pages/Tours.jsx
+++ frontend/src/pages/Tours.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import CommonSection from "../shared/CommonSection";
 
+import "../styles/tour.css";
+import tourData from "../assets/data/tours";
+import TourCard from "./../shared/TourCard";
+import SearchBar from "./../shared/SearchBar";
+import Newsletter from "./../shared/Newsletter";
+
 const Tours = () => {
   return (
     <>
