83563d80555c0c0321c167e2ed27e3c16bda60cd
Billyrosecodes <tolulopeadeyemi2020@gmail.com>
2023-09-03 19:59:21 +0100

    adding in the form for the booking

--- frontend/src/components/Booking/Booking.jsx
+++ frontend/src/components/Booking/Booking.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./booking.css";
 import { Form, FormGroup, ListGroup, ListGroupItem, Button } from "reactstrap";
 
-const Booking = ({ tour }) => {
+const Booking = ({ tour, reviews }) => {
   const { price, reviews } = tour;
 
   return (
@@ -11,6 +11,25 @@ const Booking = ({ tour }) => {
         <h3>
           ${price} <span>/per person</span>
         </h3>
+        <span className="tour_rating d-flex align-items-center">
+          <i class="ri-star-fill"></i>
+          {avgRating === 0 ? null : avgRating} ({reviews?.length})
+        </span>
+      </div>
+
+      {/* ========= Booking form========= */}
+
+      <div className="booking_form">
+        <FormGroup>
+            <input type="text" placeholder="Full Name" id="fullname" required onChange={handleChange} />
+        </FormGroup>
+        <FormGroup>
+            <input type="number" placeholder="Phone" id="phone" required onChange={handleChange} />
+        </FormGroup>
+        <FormGroup>
+            <input type="date" placeholder="" id="bookAt" required onChange={handleChange} />
+            <input type="number" placeholder="" id="guestSize" required onChange={handleChange} />
+        </FormGroup>
       </div>
     </div>
   );
