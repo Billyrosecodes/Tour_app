8e2368f61206dbd1665a4567cde77d3bc1750d7d
Billyrosecodes <tolulopeadeyemi2020@gmail.com>
2023-09-04 13:13:28 +0100

    updating the login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Row, Col, Form, Formgroup, Button } from "reactstrap";
+import { Container, Row, Col, Form, FormGroup, Button } from "reactstrap";
 import {Link} from 'react-router-dom';
 import '../styles/login.css';
 
@@ -12,7 +12,29 @@ const Login = () => {
     <Container>
       <Row>
         <Col lg='8' className='m-auto'>
-           <div className="login_container d-flex justify-content-between"></div>
+           <div className="login_container d-flex justify-content-between">
+            <div className="login_img">
+              <img src={loginImg} alt="" />
+            </div>
+
+            <div className="login_form">
+              <div className="user">
+                <img src={userIcon} alt="" />
+              </div>
+              <h2>Login</h2>
+
+              <Form>
+                <FormGroup>
+                  <input type="email" placeholder='Email' required id='email' onChange={handleChange} />
+                </FormGroup>
+                <FormGroup>
+                  <input type="password" placeholder='Password' required id='password' onChange={handleChange} />
+                </FormGroup>
+                <Button className='btn secondary_btn auth_btn' type='submit'>Login</Button>
+              </Form>
+              <p>Don't have an account?<Link to='/register'>Create</Link></p>
+            </div>
+           </div>
         </Col>
       </Row>
     </Container>
