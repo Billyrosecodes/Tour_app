4955c09dada034af02799ea33fa393e3c3408e6a
Billyrosecodes <tolulopeadeyemi2020@gmail.com>
2023-10-16 16:06:34 +0100

    debugging

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 8000
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
-
+ 
 app.listen(port, ()=>{
     console.log('server listening on port', port);
 })
