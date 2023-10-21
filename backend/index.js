3347659e4e298f20a5fb11543161e0ded1b0dd49
Billyrosecodes <tolulopeadeyemi2020@gmail.com>
2023-10-21 01:53:15 +0100

    server set up and running

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,24 @@
 import express from 'express';
-import dotenv from 'dotenv'
-import mongoose from 'mongoose'
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 import cors from 'cors'
-import cookieParser from 'cookie-parser'
+import cookieParser from 'cookie-parser';
 
 dotenv.config()
 const app = express()
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 8000;
 
-
+//for testing
+app.get('/', (req, res) => {
+    res.send("api is working");
+});
 
 //middleware
-app.use(express.json());
-app.use(cors());
-app.use(cookieParser());
- 
-app.listen(port, ()=>{
-    console.log('server listening on port', port);
-})
+app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
+
+
+app.listen(port, () => {
+    console.log("server listening on port", port);
+});
