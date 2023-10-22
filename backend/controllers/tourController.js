454bb18a86cd6e853bd6289ccbaaaaf549197113
Billyrosecodes <tolulopeadeyemi2020@gmail.com>
2023-10-22 21:03:39 +0100

    setting up crud operation

--- backend/controllers/tourController.js
+++ backend/controllers/tourController.js
@@ -1,14 +1,21 @@
-import Tour from '../models/Tour.js'
-
+import Tour from "../models/Tour.js";
 
 //create a new tour
 export const createTour = async (req, res) => {
-    const newTour = new Tour(req.body)
+  const newTour = new Tour(req.body);
 
-    try {
-        const savedTour = await newTour.save()
-        res.status(200).json({success:true, message:'Successfully created', data:savedTour})
-    } catch (err) {
-        res.status(500).json({success:false, message:'Failed to create. Try again'})
-    }
-}
+  try {
+    const savedTour = await newTour.save();
+    res
+      .status(200)
+      .json({
+        success: true,
+        message: "Successfully created",
+        data: savedTour,
+      });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ success: false, message: "Failed to create. Try again" });
+  }
+};
