79c3467add310120b35e14dd44c5da51e83ac9f7
Billyrosecodes <tolulopeadeyemi2020@gmail.com>
2023-10-23 00:12:08 +0100

    fixing bug with the update tour

--- backend/controllers/tourController.js
+++ backend/controllers/tourController.js
@@ -44,9 +44,20 @@ export const updateTour = async (req, res) => {
 }
 //delete tour
 export const deleteTour = async (req, res) => {
+    const id = req.params.id
+
     try {
-        
+     await Tour.findByIdAndDelete(id)
+
+     res.status(200).json({
+       success: true,
+       message: "Successfully deleted",
+     });
     } catch (err) {
+        res.status(500).json({
+          success: false,
+          message: "failed to delete",
+        });
         
     }
 }
