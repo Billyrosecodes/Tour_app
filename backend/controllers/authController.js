6b730690641a0e74c3b0a38bf57ef4b5c5d62d41
Billyrosecodes <tolulopeadeyemi2020@gmail.com>
2023-10-25 20:51:16 +0100

    implemented password hashing

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,14 +1,19 @@
 import User from "../models/User.js";
-import bcyrpt from "bcryptjs";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
 // user registration
 export const register = async (req, res) => {
   try {
+
+      // hashing password
+      const salt = bcrypt.genSaltSync(10);
+      const hash = bcrypt.hashSync(req.body.password, salt);
+
     const newUser = new User({
       username: req.body.username,
       email: req.body.email,
-      password: req.body.password,
+      password: hash,
       photo: req.body.photo,
     });
 
