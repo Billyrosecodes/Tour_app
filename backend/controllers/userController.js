d2f2a6f189baeafa93af1be17e703e1410f97aa7
Billyrosecodes <tolulopeadeyemi2020@gmail.com>
2023-10-25 18:34:17 +0100

    setting up routes for the users

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -85,13 +85,10 @@ export const getSingleUser = async (req, res) => {
 
 //get All user
 export const getAllUser = async (req, res) => {
-     try {
-    const users = await User.find({}) 
-   
-    res.status(200).json({ success: true, 
-        message: "Successful",
-        data: users,
-    });
+  try {
+    const users = await User.find({});
+
+    res.status(200).json({ success: true, message: "Successful", data: users });
   } catch (err) {
     res.status(404).json({
       success: false,
