963231f73d319ba05c3cf9a103a5bfe1ed009ee9
Billyrosecodes <tolulopeadeyemi2020@gmail.com>
2023-10-25 20:10:16 +0100

    adding in auth.js

---
+++ backend/controllers/authController.js
@@ -0,0 +1,21 @@
+import User from "../models/User.js";
+
+
+// user registration
+export const register = async (req, res) => {
+    try {
+        
+    } catch (err) {
+        
+    }
+}
+
+
+// user login
+export const login = async (req, res) => {
+    try {
+        
+    } catch (err) {
+        
+    }
+}
