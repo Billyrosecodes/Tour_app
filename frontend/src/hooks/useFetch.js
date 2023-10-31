4021dc985f31586b43d41ecdf906ca9fd6227e7c
Billyrosecodes <tolulopeadeyemi2020@gmail.com>
2023-10-31 14:55:50 +0100

    passed review data

--- frontend/src/hooks/useFetch.js
+++ frontend/src/hooks/useFetch.js
@@ -1,12 +1,36 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-const useFetch = () => {
-    const [data, setData] = useState([])
-    const [error, setError] = useState(null)
-    const [loading, setLoading] = useState(false)
+const useFetch = (url) => {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const res = await fetch(url);
+        if (!res.ok) {
+          setError("failed to fetch");
+        }
+        const result = await res.json();
+        
+        setData(result.data);
+        setLoading(false);
+      } catch (err) {
+        setError(err.message);
+        setLoading(false);
+      }
+    };
 
-    return
-}
+    fetchData();
+  }, [url]);
 
-export default useFetch 
+  return {
+    data,
+    error,
+    loading,
+  };
+};
+
+export default useFetch;
