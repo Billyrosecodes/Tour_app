088397be090e725d6d07b293dd52f77a1f5c1b26
Billyrosecodes <tolulopeadeyemi2020@gmail.com>
2023-11-11 17:34:49 +0100

    fixed review

--- frontend/src/pages/TourDetails.jsx
+++ frontend/src/pages/TourDetails.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState, useContext } from "react";
 import "../styles/tour-details.css";
 import { Container, Row, Col, Form, ListGroup } from "reactstrap";
 import { useParams } from "react-router-dom";
@@ -8,11 +8,14 @@ import Booking from "../components/Booking/Booking";
 import Newsletter from "./../shared/Newsletter";
 import useFetch from './../hooks/useFetch';
 import { BASE_URL } from "./../utils/config";
+import {AuthContext} from "./../context/AuthContext"
 
 const TourDetails = () => {
   const { id } = useParams();
   const reviewMsgRef = useRef("");
   const [tourRating, setTourRating] = useState(null);
+  const {user} = useContext(AuthContext)
+
 
   // fetch data from database
   const { data: tour, loading, error } = useFetch(`${BASE_URL}/tours/${id}`);
@@ -36,16 +39,44 @@ const TourDetails = () => {
   const options = { day: "numeric", month: "long", year: "numeric" };
 
   //submit request to the server
-  const submitHandler = (e) => {
+  const submitHandler = async (e) => {
     e.preventDefault();
     const reviewText = reviewMsgRef.current.value;
 
-    // later will call api
+   try {
+    if(!user || user === undefined || user ===null){
+      alert('Please sign in')
+    }
+
+    const reviewObj = {
+      username: user?.username,
+      reviewText,
+      rating: tourRating
+    }
+
+    const res = await fetch(`${BASE_URL}/review/${id}`,{
+      method:'post',
+      headers: {
+        'content-type': 'application/json'
+      },
+      credentials: 'include',
+      body:JSON.stringify(reviewObj),
+    });
+
+    const result = await res.json()
+    if(!res.ok) {
+      return alert(result.message);
+    }
+
+    alert(result.message)
+   } catch (err) {
+    alert(err.message);
+   }
   };
  
   useEffect(() => {
-    window.scrollTo(0,0)
-  },[tour])
+    window.scrollTo(0, 0)
+  }, [tour])
 
   return (
     <>
@@ -147,18 +178,19 @@ const TourDetails = () => {
                       <div className="w-100">
                         <div className="d-flex align-items-center justify-content-between">
                           <div>
-                            <h5>Tolu</h5>
+                            <h5>{review.username}</h5>
                             <p>
-                              {new Date("09-02-2023").toLocaleDateString(
+                              {new Date(review.createdAt).toLocaleDateString(
                                 "en-US",
                                 options
                               )}
                             </p>
                           </div>
                           <span className="d-flex align-items-center">
-                            5 <i className="ri-star-s-fill"></i>
+                            {review.rating}
+                          <i className="ri-star-s-fill"></i>
                           </span>
-                          <h6>Amazing tour</h6>
+                          <h6>{review.reviewText}</h6>
                         </div>
                       </div>
                     </div>
